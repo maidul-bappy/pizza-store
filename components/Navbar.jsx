a6d4558a560db44361ed63edb316947f4c6d124e
maidul.bappy <maidul.bappy.001@gmail.com>
2022-01-09 02:57:38 +0600

    navbar designed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
+import cart from "/public//img/cart.png";
+import logo from "/public//img/logo.png";
 import telephone from "/public//img/telephone.png";
 
 const Navbar = () => {
@@ -14,8 +16,23 @@ const Navbar = () => {
           <div className={styles.text}>012 345 678</div>
         </div>
       </div>
-      <div className={styles.item}>center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src={logo} alt="logo" width="70px" height="70px" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src={cart} alt="logo" width="30px" height="30px" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
